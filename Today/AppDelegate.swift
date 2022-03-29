c373a5f5abeeca4d297d51fc0de63b34be8e514b
higekick <higekick39@gmail.com>
2022-03-29 17:58:29 +0900

    Chapter2-4 Displaying Reminder Details - https://developer.apple.com/tutorials/app-dev-training/displaying-reminder-details

--- Today/AppDelegate.swift
+++ Today/AppDelegate.swift
@@ -11,9 +11,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().tintColor = .todayPrimaryTint
+        UINavigationBar.appearance().backgroundColor = .todayNavigationBackground
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+//        navBarAppearance.configureWithTransparentBackground()
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
         return true
     }
 
